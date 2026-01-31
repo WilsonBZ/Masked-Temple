6f94c2e228a7644ddf2d83767f4425af663368fb
WilsonBZ <73350635+WilsonBZ@users.noreply.github.com>
2026-01-31 12:18:52 +0800

    e

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -16,6 +16,6 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        //mainCamera.ScreenToWorldPoint(Input.mousePosition);
     }
 }
