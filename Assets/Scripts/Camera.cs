8e2af12aaea0ec9baa5a98c20f20c2251715bf71
Yyct <lwlwaich1998@gmail.com>
2026-01-31 12:13:30 +0800

    Prefabs

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    [SerializeField] private float width;
+    [SerializeField] private float height;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainCamera.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
